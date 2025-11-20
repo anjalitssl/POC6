f34dc4e53c042a38960f0d40d3a531b73bee87df
gfdsxgf <gfsgrshshgsrs.com>
2025-11-20 11:20:28 +0530

    late

---
+++ src/main/java/com/anjali/poc/Poc6Application.java
@@ -0,0 +1,12 @@
+package com.anjali.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Poc6Application.class, args);
+    }
+}
